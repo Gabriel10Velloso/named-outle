02dd44bddcd496ecef58a34d47b70630a2f40413
Gabriel10Velloso <spiderconnected@gmail.com>
2023-06-26 15:19:08 -0300

    Alteração teste rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,20 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  link1 = ['/', { outlets: {
-    'outlet-a': ['page-red'], // en função do mapToAllOutlets bata apenas alterar a rota
-    'outlet-b': ['page-green'],// altere page-red que a mágica acontece
-    'outlet-c': ['page-blue']
-  }}];
-  link2 = ['/', { outlets: {
-    'outlet-a': ['page-red'],
-    'outlet-b': ['page-blue'],
-    'outlet-c': ['page-green']
-  }}];
+
+  constructor(private router: Router, private activateRoute: ActivatedRoute) { }
+  link1: any = ['/', {
+    outlets: {
+      'outlet-a': ['page-red'], // en função do mapToAllOutlets bata apenas alterar a rota
+      'outlet-b': ['page-green'],// altere page-red que a mágica acontece
+      'outlet-c': ['page-blue']
+    }
+  }];
+  link2 = ['/', {
+    outlets: {
+      'outlet-a': ['page-red'], 'outlet-b': ['page-blue'], 'outlet-c': ['page-green']
+    }
+  },];
 
 
 
+  home() {
+    // this.router.navigate([{ outlets: { 'outlet-c': [ 'page-blue'],  }}]);
+    // this.router.navigate(['/',{ outlets: { 'outlet-c': ['page-blue'] } }], { skipLocationChange: true });
+    // this.link1 = ['/', { outlets: { 'outlet-c': ['page-blue']}}];
 
+    // this.router.navigate(['/'])
+    // this.router.navigate(['/',{ outlets: { 'outlet-c': ['page-blue'] } }], { skipLocationChange: true });
+    // this.router.navigate(['/',{ outlets: { 'outlet-c': ['page-blue'] } }],);
+    // setTimeout(()=>{
+    //   this.router.navigate(['/',{ outlets: { 'outlet-c': ['page-blue'] } }],);
+    // },200)
+
+    // this.router.navigate(['/', { outlets: { 'outlet-c': null } }], );
+    this.router.navigate(['/', {
+      outlets: {
+        'outlet-a': null,
+        'outlet-b': null,
+        'outlet-c': null
+      }
+    }], {
+      // relativeTo: this.activateRoute.parent
+    });
+  }
 
 
 
--- src/app/urlSerializer.ts
+++ src/app/urlSerializer.ts
@@ -3,16 +3,20 @@ import { DefaultUrlSerializer, UrlTree } from "@angular/router";
 export class CustomUrlSerializer extends DefaultUrlSerializer {
   private _reverseUrl(url: string): string {
     const startIndex = 1;
-    const segmentString =
-      `(${url.substring(startIndex).split('/').join('//')})`;
+    const segmentString = `(${url.substring(startIndex).split('/').join('//')})`;
     return url.substring(0, startIndex) + segmentString;
   }
   private _beautifyUrl(url: string): string {
+
     // console.log('>>>>',url)
-    return url
-      .replace('(', '')
-      .replace(')', '')
-      .split('//').join('/');
+    let newUrl = url.replace('(', '')
+              .replace(')', '')
+              .split('//').join('/')
+              // .replace('outlet-a:', '')
+
+    // console.log(newUrl)
+    return newUrl
+
   }
   override parse(url: string): UrlTree {
     return super.parse(this._reverseUrl(url));
@@ -21,3 +25,48 @@ export class CustomUrlSerializer extends DefaultUrlSerializer {
     return this._beautifyUrl(super.serialize(tree));
   }
 }
+
+
+
+// import { DefaultUrlSerializer, UrlTree } from "@angular/router";
+
+// export class CustomUrlSerializer extends DefaultUrlSerializer {
+//   private _reverseUrl(url: string): string {
+//     const startIndex = 1;
+//     let segmentString = `(${url.substring(startIndex).split('/').join('//')})`;
+
+
+//     if (segmentString !== '()') {
+//       let z = `ooutlet-a:${url}`
+//       segmentString = `(${z.substring(startIndex).split('/').join('')})`;
+//       return url.substring(0, startIndex) + segmentString;
+//     }
+
+//     return url.substring(0, startIndex) + segmentString;
+
+//     // const startIndex = 1;
+//     // let z = `ooutlet-a:${url}`
+//     // console.log('qqqqqqqqq', `(${z.substring(startIndex).split('/').join('')})`)
+//     // console.log(`(${url.substring(startIndex).split('/').join('//')})`)
+//     // const segmentString = `(${z.substring(startIndex).split('/').join('')})`;
+//     // return url.substring(0, startIndex) + segmentString;
+//   }
+//   private _beautifyUrl(url: string): string {
+
+//     // console.log('>>>>',url)
+//     let newUrl = url.replace('(', '')
+//       .replace(')', '')
+//       .split('//').join('/')
+//       .replace('outlet-a:', '')
+
+//     // console.log(newUrl)
+//     return newUrl
+
+//   }
+//   override parse(url: string): UrlTree {
+//     return super.parse(this._reverseUrl(url));
+//   }
+//   override serialize(tree: UrlTree): string {
+//     return this._beautifyUrl(super.serialize(tree));
+//   }
+// }
